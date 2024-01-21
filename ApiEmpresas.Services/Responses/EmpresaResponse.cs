54421818bb6c377cf164d6b4b428257cb0fec2d3
Antonio Barbosa <acrb1108@gmail.com>
2024-01-21 15:49:06 -0300

    Criando as Requests e Responses

---
+++ ApiEmpresas.Services/Responses/EmpresaResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiEmpresas.Services.Responses
+{
+    /// <summary>
+    /// Modelagem de dados de retorno da empresa na API
+    /// </summary>
+    public class EmpresaResponse
+    {
+        public Guid Id { get; set; }
+        public string? NomeFantasia { get; set; }
+        public string? RazaoSocial { get; set; }
+        public string? Cnpj { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public DateTime DataUltimaAlteracao { get; set; }
+    }
+}
