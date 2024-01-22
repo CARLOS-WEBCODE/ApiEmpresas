c2ec9010b157e51527b1a02ad9a081794462f54b
Antonio Barbosa <acrb1108@gmail.com>
2024-01-22 16:13:44 -0300

    Criando o mapeamento Empresa e Funcionario.

--- ApiEmpresas.Infra.Data/Mappings/EmpresaMap.cs
+++ ApiEmpresas.Infra.Data/Mappings/EmpresaMap.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ApiEmpresas.Infra.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,38 @@ using System.Threading.Tasks;
 
 namespace ApiEmpresas.Infra.Data.Mappings
 {
-    public class EmpresaMap
+    /// <summary>
+    /// Classe de mapeamento ORM para a entidade Empresa
+    /// </summary>
+    public class EmpresaMap : IEntityTypeConfiguration<Empresa>
     {
+        ///Método para fazer o mapeamento da entidade
+        public void Configure(EntityTypeBuilder<Empresa> builder)
+        {
+            //nome da tabela
+            builder.ToTable("EMPRESA");
 
+            //chave primária
+            builder.HasKey(e => e.IdEmpresa);
+
+            //mapeamento os campos da tabela
+            builder.Property(e => e.IdEmpresa)
+                .HasColumnName("IDEMPRESA");
+
+            builder.Property(e => e.NomeFantasia)
+                .HasColumnName("NOMEFANTASIA")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(e => e.RazaoSocial)
+                .HasColumnName("RAZAOSOCIAL")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(e => e.Cnpj)
+                .HasColumnName("CNPJ")
+                .HasMaxLength(20)
+                .IsRequired();
+        }
     }
 }
--- ApiEmpresas.Infra.Data/Mappings/FuncionarioMap.cs
+++ ApiEmpresas.Infra.Data/Mappings/FuncionarioMap.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ApiEmpresas.Infra.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,46 @@ using System.Threading.Tasks;
 
 namespace ApiEmpresas.Infra.Data.Mappings
 {
-    public class FuncionarioMap
+    ///Classe de mapeamento para a entidade Funcionario
+    public class FuncionarioMap : IEntityTypeConfiguration<Funcionario>
     {
+        ///Método para mapear a entidade
+        public void Configure(EntityTypeBuilder<Funcionario> builder)
+        {
+            //nome da tabela
+            builder.ToTable("FUNCIONARIO");
 
+            //chave primária
+            builder.HasKey(f => f.IdFuncionario);
+
+            //mapeamento dos campos da entidade
+            builder.Property(f => f.IdFuncionario)
+                .HasColumnName("IDFUNCIONARIO")
+                .IsRequired();
+
+            builder.Property(f => f.Nome)
+                .HasColumnName("NOME")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(f => f.Cpf)
+                .HasColumnName("CPF")
+                .HasMaxLength(14)
+                .IsRequired();
+
+            builder.Property(f => f.Matricula)
+                .HasColumnName("MATRICULA")
+                .HasMaxLength(10)
+                .IsRequired();
+
+            builder.Property(f => f.DataAdmissao)
+                .HasColumnName("DATAADMISSAO")
+                .HasColumnType("date")
+                .IsRequired();
+
+            builder.Property(f => f.IdEmpresa)
+                .HasColumnName("IDEMPRESA")
+                .IsRequired();
+        }
     }
 }
