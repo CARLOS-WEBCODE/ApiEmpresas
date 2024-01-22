469b9dc5f257e5fbd3bec85763c30b1ca0175e56
Antonio Barbosa <acrb1108@gmail.com>
2024-01-22 12:02:10 -0300

    Criando as classes EmpresaMap e FuncionarioMap.

---
+++ ApiEmpresas.Infra.Data/Mappings/FuncionarioMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiEmpresas.Infra.Data.Mappings
+{
+    public class FuncionarioMap
+    {
+
+    }
+}
