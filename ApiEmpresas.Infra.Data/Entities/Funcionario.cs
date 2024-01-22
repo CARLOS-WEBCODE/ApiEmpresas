0d9330cb0eb5a2e547955d2bed4db30e039a492c
Antonio Barbosa <acrb1108@gmail.com>
2024-01-22 11:30:13 -0300

    Atualizando a entiti classe Funcionario

--- ApiEmpresas.Infra.Data/Entities/Funcionario.cs
+++ ApiEmpresas.Infra.Data/Entities/Funcionario.cs
@@ -8,5 +8,21 @@ namespace ApiEmpresas.Infra.Data.Entities
 {
     public class Funcionario
     {
+        #region Propriedades
+
+        public Guid IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Matricula { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public Guid IdEmpresa { get; set; }
+
+        #endregion
+
+        #region Relacionamentos
+
+        public Empresa Empresa { get; set; }
+
+        #endregion
     }
 }
