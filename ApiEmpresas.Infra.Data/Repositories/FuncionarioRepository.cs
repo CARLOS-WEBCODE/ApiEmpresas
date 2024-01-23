35aa0f2092417523abcfbe5995e81e2c0e11a519
Antonio Barbosa <acrb1108@gmail.com>
2024-01-23 09:58:42 -0300

    Implementando o repositorio.

---
+++ ApiEmpresas.Infra.Data/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,54 @@
+﻿using ApiEmpresas.Infra.Data.Entities;
+using ApiEmpresas.Infra.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiEmpresas.Infra.Data.Repositories
+{
+    public class FuncionarioRepository : IFuncionarioRepository
+    {
+
+        public void Inserir(Funcionario entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(Funcionario entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Funcionario entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Funcionario> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Funcionario ObterPorCpf(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Funcionario ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Funcionario ObterPorMatricula(string matricula)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Funcionario> ObterPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
