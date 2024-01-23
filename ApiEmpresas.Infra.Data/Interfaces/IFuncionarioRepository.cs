f1e931c921765656f6459faea9471cea332d1ded
Antonio Barbosa <acrb1108@gmail.com>
2024-01-23 08:54:45 -0300

    Criando a interfaces IEmpresaRepositpry e IFuncionarioRepository.

---
+++ ApiEmpresas.Infra.Data/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,30 @@
+﻿using ApiEmpresas.Infra.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiEmpresas.Infra.Data.Interfaces
+{
+    /// <summary>
+    /// Interface de repositorio para operações de funcionário
+    /// </summary>
+    public interface IFuncionarioRepository : IBaseRepository<Funcionario>
+    {
+        /// <summary>
+        /// Método para retornar 1 funcionário baseado no cpf
+        /// </summary>
+        Funcionario ObterPorCpf(string cpf);
+
+        /// <summary>
+        /// Método para retornar 1 funcionário baseado na matricula
+        /// </summary>
+        Funcionario ObterPorMatricula(string matricula);
+
+        /// <summary>
+        /// Método para retornar funcionários baseado no nome
+        /// </summary>
+        List<Funcionario> ObterPorNome(string nome);
+    }
+}
