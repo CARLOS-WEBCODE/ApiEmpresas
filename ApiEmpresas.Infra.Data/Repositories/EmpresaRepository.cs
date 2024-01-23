5c0fe365ff772666ed400694de62e2320f943d1b
Antonio Barbosa <acrb1108@gmail.com>
2024-01-23 09:00:54 -0300

    Implementando as interfaces no EmpresaRepository : IEmpresaRepository

---
+++ ApiEmpresas.Infra.Data/Repositories/EmpresaRepository.cs
@@ -0,0 +1,44 @@
+﻿using ApiEmpresas.Infra.Data.Entities;
+using ApiEmpresas.Infra.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiEmpresas.Infra.Data.Repositories
+{
+    public class EmpresaRepository : IEmpresaRepository
+    {
+        public void Inserir(Empresa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(Empresa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Empresa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empresa> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empresa ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empresa ObterPorCnpj(string cnpj)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
