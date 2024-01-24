f6b0940e252e36e297973b90b70de5ae43f30587
Antonio Barbosa <acrb1108@gmail.com>
2024-01-24 11:37:34 -0300

    Atualizando o EmpresaResponse

--- ApiEmpresas.Services/Responses/EmpresaResponse.cs
+++ ApiEmpresas.Services/Responses/EmpresaResponse.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public class EmpresaResponse
     {
-        public Guid Id { get; set; }
+        public Guid IdEmpresa { get; set; }
         public string? NomeFantasia { get; set; }
         public string? RazaoSocial { get; set; }
         public string? Cnpj { get; set; }
