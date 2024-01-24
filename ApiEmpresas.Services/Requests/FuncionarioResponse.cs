23512ed8426353b0d05bca99c0c07583415a6d3d
Antonio Barbosa <acrb1108@gmail.com>
2024-01-24 11:55:49 -0300

    Atualizando FuncionarioResponse.

---
+++ ApiEmpresas.Services/Requests/FuncionarioResponse.cs
@@ -0,0 +1,23 @@
+﻿using ApiEmpresas.Services.Responses;
+
+namespace ApiEmpresas.Services.Requests
+{
+    /// <summary>
+    /// Modelagem de dados de retorno de funcionário na API
+    /// </summary>
+    public class FuncionarioResponse
+    {
+        public Guid IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Matricula { get; set; }
+        public DateTime DataAdmissao { get; set; }
+
+        #region Relacionamento
+
+        public EmpresaResponse Empresa { get; set; }
+
+        #endregion
+
+    }
+}
