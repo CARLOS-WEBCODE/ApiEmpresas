b8c9b0807a552e5c0baac6c06eee5f17a6f1e985
Antonio Barbosa <acrb1108@gmail.com>
2024-01-25 10:52:05 -0300

    Criando a classe Criptografia

---
+++ ApiEmpresas.Services/Requests/RegisterPostRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiEmpresas.Services.Requests
+{
+    public class RegisterPostRequest
+    {
+        [Required(ErrorMessage = "Informe o nome do usuário.")]
+        public string Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Informe um email válido.")]
+        [Required(ErrorMessage = "Informe o email do usuário")]
+        public string Email { get; set; }
+
+        [MinLength(8, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(20, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Informe a senha do usuário")]
+        public string Senha { get; set; }
+    }
+}
