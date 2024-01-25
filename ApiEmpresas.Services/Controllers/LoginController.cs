ec2cc5477a9f9e4bfc3c82fc097f88bd27b88d27
Antonio Barbosa <acrb1108@gmail.com>
2024-01-25 11:42:07 -0300

    Atualizando LoginController, LoginPostRequest

--- ApiEmpresas.Services/Controllers/LoginController.cs
+++ ApiEmpresas.Services/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using ApiEmpresas.Services.Authorization;
+﻿using ApiEmpresas.Infra.Data.Interfaces;
+using ApiEmpresas.Services.Authorization;
 using ApiEmpresas.Services.Requests;
+using ApiEmpresas.Services.Utils;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,21 +13,44 @@ namespace ApiEmpresas.Services.Controllers
     {
         //atributo
         private readonly JwtService _jwtService;
+        private readonly IUnitOfWork _unitOfWork;
 
         //Construtor para injeção de dependência
-        public LoginController(JwtService jwtService)
+        public LoginController(JwtService jwtService, IUnitOfWork unitOfWork)
         {
             _jwtService = jwtService;
+            _unitOfWork = unitOfWork;
         }
 
         [HttpPost]
         public IActionResult Post(LoginPostRequest request)
         {
-            if (request.Email.Equals("admin@admin.com") && request.Senha.Equals("coti"))
-                return StatusCode(200, _jwtService.GenerateToken("admin"));
-            else
-                return StatusCode(401); //UNAUTHORIZED (Acesso Negado)
+            try
+            {
+                //obter o usuário no banco de dados
+                var usuario = _unitOfWork.UsuarioRepository.Obter(request.Email, Criptografia.GetMD5(request.Senha));
+
+                //verificar se o usuário foi encontrado
+                if (usuario != null)
+                {
+                    return StatusCode(200, new
+                    {
+                        message = "Autenticação realizada com sucesso.",
+                        usuario = usuario.Email,
+                        token = _jwtService.GenerateToken(usuario.Email)
+                    });
+                }
+                else
+                {
+                    return StatusCode(401, new { message = "Acesso negado. Usuário inválido." });
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
     }
+    
 }
